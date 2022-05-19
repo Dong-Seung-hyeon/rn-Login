ebab6ec6adf54b71c57c6183f8d5ca806abcb929
seunghyeondong <kj94gakt@naver.com>
2022-05-19 16:48:57 +0900

    포커스 상태관리를 위해 Input.jsx에서 코드수정을 해주었다.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+//포커스 상태를 관리하기 위해서 useState를 사용했다.
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
 
@@ -12,7 +13,8 @@ const Label = styled.Text` //Label 컨포넌트도 작성해주었다.
     font-size: 14px;
     font-weight: 600;
     margin-bottom: 6px;
-    color: ${({theme}) => theme.inputLabel};
+    color: ${({theme, isFocused}) => isFocused ? theme.text : theme.inputLabel};
+    //Label 컨포넌트에서 isFocused 값에 따라 다른 스타일이 적용되도록 코드를 수정하였다.
 `;
 
 const StyledInput = styled.TextInput.attrs(({theme}) => ({ 
@@ -27,14 +29,15 @@ const StyledInput = styled.TextInput.attrs(({theme}) => ({
     color: ${({theme}) => theme.text};
     padding: 20px 10px;
     font-size: 16px;
-    border: 1px solid ${({theme}) => theme.inputBorder};
+    border: 1px solid ${({theme, isFocused}) => isFocused ? theme.text : theme.inputBorder};
+    //Label 컨포넌트에서 isFocused 값에 따라 다른 스타일이 적용되도록 코드를 수정하였다.
     border-radius: 4px;
 `;
 
 const Input = ({
     //Input 컨포넌트는 props로 
-    label, //라벨
-    value, //가치
+    label,
+    value,
     onChangeText, //Text가 변화할 때마다 불리는 function이다.
     onSubmitEditing, //Text가 보내질 때마다 불리어지는 function이다.
     onBlur,
@@ -44,24 +47,43 @@ const Input = ({
     //안드로이드 전용 => (none, previous)가 있고, ios 전용 => (default, emergency-call, google, join, route, yahoo)등이 있다. 
     maxLength, //너무 긴 text가 입력되지 않도록 하는 코드이다.
 }) => {
+    const [isFocused, setIsFocused] = useState(false);
+    //포커스된 상태와 포커스되지 않은 상태를 구분하기위해서 사용한 코드이다.
+    //포커스 상태를 관리하기 위해서 useState를 가져오고, isFocused라는 상태변수를 만들었다.
+
     return ( //props로 전달된 값들을 이용하여 label과 input 컨포넌트에 각각 설정해주었다.
         <Container>
-        <Label>{label}</Label>
+        <Label isFocused={isFocused}>{label}</Label>
+        //isFocused 상태변수를 Label 컨포넌트로 전달하여 상태에 따라 스타일이 다르게 나타나도록 하기위해 수정해주었다.
         <StyledInput
         value={value}
         onChangeText={onChangeText}
         onSubmitEditing={onSubmitEditing}
-        onBlur={onBlur}
+        onBlur={() => {
+            setIsFocused(false);
+            // 포커스를 잃었을때 업데이트를 하기위해서 추가를해주었다.
+            onBlur();
+        }}
         placeholder={placeholder}
         returnKeyType={returnKeyType}
         maxLength={maxLength}
         autoCapitalize="none" //자동 대문자 기능이나, 오타수정기능에 관련된 설정도 함께해주었다.
         autoCorrect={false}
         textContentType="none" //ios에서 이메일이 나타내는 부분을 없애기 위해서 사용했다.
+        isFocused={isFocused} 
+        //isFocused 상태변수를 StyledInput 컨포넌트로 전달하여 상태에 따라 스타일이 다르게 나타나도록 하기위해 수정해주었다.
+        onFocus={()=>setIsFocused(true)}
+        //isFocused의 값은 onFocus가 호출이 되었을때 변경되도록 수정해주었다.
         />
         </Container>
     )
 };
+
+Input.defaultProps = {
+    onBlur: ()=>{},
+    // onBlur가 호출이 될때 props로 전달된 onBlur가 호출이될때 아무것도 전달이되지 않았을때 문제가 발생할 수 있다.
+    // 그래서 defaultProps를 설정하여 혹시 모를 문제에 대응하도록 하였다. 
+}
 
 Input.propTypes = { //props로 전달되는 값들은 PropTypes를 이용하여 type을 설정한다.
     label: PropTypes.string,
