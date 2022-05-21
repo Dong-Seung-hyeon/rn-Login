ddebe861c7f1bf64c557170ee0c4cb7e5e1c9c5d
seunghyeondong <kj94gakt@naver.com>
2022-05-21 20:31:14 +0900

    ErrorMessage.jsx파일에서 height를 잘못입력하여서 수정해주었다.

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const StyledText = styled.Text`
     align-items: flex-start;
     width: 100%;
-    height: 100%;
+    height: 20px;
     margin-bottom: 10px;
     line-height: 20px;
     color: ${({theme}) => theme.errorText};
