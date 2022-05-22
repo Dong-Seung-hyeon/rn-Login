53d7c7b74f011ffd022a3aca1ec544c8a0dbef0c
seunghyeondong <kj94gakt@naver.com>
2022-05-22 13:00:16 +0900

    Profile화면에 signup버튼이 클릭이되면 UserContext에 setUser를 이용해서 User정보를 변경하도록 만들었다.

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, {useContext} from 'react';
+import {UserContext} from 'styled-components/native'; 
 import {Button, Image} from '../components';
-import { ScrollView } from 'react-native';
-
-  const Icon = 'https://firebasestorage.googleapis.com/v0/b/rn-catch-my-hand.appspot.com/o/icon.png?alt=media'
 
 const Container = styled.View`
     flex: 1;
@@ -13,13 +10,13 @@ const Container = styled.View`
 const Profile = ({navigation, route}) => {
     console.log(route.params);
     /*전송된 유저의 정보를 console.log를 이용하여 확인하도록 하였다.*/
+    const {setUser} = useContext(UserContext);
+    /* Profile화면에 signup버튼이 클릭이되면 UserContext에 setUser를 이용해서 User정보를 변경하도록 만들었다. */
+
     return (
-        <ScrollView extraScrollHeight={30} contentContainerStyle={{flex:1}}>
         <Container>
-            <Image source = {require('../screens/dog.png')} />
-            <Button title="로그아웃" onPress={() => navigation.navigate('Signin')}/>
+            <Button title="로그아웃" onPress={() => setUser({})}/>
         </Container>
-        </ScrollView>
     );
 };
 
