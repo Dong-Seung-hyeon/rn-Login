ca0e59ad520a1dfdf8daa0a8af3de97a5c096f4f
seunghyeondong <kj94gakt@naver.com>
2022-05-30 15:37:08 +0900

    제목을 관리할 title 상태변수, 설명을 관리할 description 상태변수, 에러메시지를 관리할 error 상태변수, 버튼의 활성화 여부를 관리하는 disabled 상태변수, 포커스 이동을 위해 refDesc도 만들어주었다.

--- src/screens/ChannelCreation.jsx
+++ src/screens/ChannelCreation.jsx
@@ -9,16 +9,27 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 const Container = styled.View`
     flex: 1;
     background-color: white;
-`;
-
-const StyledText = styled.Text`
-    font-size: 30px;
+    justify-content: center;
+    /* Container components의 style을 수정 */
+    align-items: center;
+    padding: 0 20px;
 `;
 
 const ChannelCreation = ({navigation}) => {
+    const [title, setTitle] = useState('');
+    /* 제목을 관리할 title 상태변수 */
+    const [desc, setDesc] = useState('');
+    /* 설명을 관리할 description 상태변수 */
+    const [errorMessage, setErrorMessage] = useState('');
+    /* 에러메시지를 관리할 error 상태변수 에러메시지는 조금 더 명확하게 errorMessage로 해주었다. */
+    const [disabled, setDisabled] = useState(true);
+    /* 버튼의 활성화 여부를 관리하는 disabled 상태변수 */
+
+    const refDesc = useRef(null);
+    /* 포커스 이동을 위해 refDesc도 만들어주었다. */
+
     return(
         <Container>
-            <StyledText>Channel Creation</StyledText>
             <Button title="Create" onPress={() => navigation.replace('Channel')/* navigate함수 대신 replace함수를 이용해 화면을 이동한다. */} />
         </Container>
     );
