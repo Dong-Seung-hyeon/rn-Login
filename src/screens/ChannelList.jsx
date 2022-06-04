7921213c183f4885ab5cb5e92519dab32310f500
seunghyeondong <kj94gakt@naver.com>
2022-06-05 02:15:48 +0900

    ChannelList.jsx에 ItemComponent, ItemTextContainer, ItemTitle, ItemDesc, ItemTime, ItemIcon을 추가해주었다.

--- src/screens/ChannelList.jsx
+++ src/screens/ChannelList.jsx
@@ -1,23 +1,106 @@
 import React from 'react';
-import styled from 'styled-components/native'
-import {Button} from '../components';
+import { FlatList } from 'react-native';
+import styled from 'styled-components/native';
+import { Button } from '../components';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const channels = []; /* channels라는 배열을 만들었다 */
+for (let i = 0; i < 1000; i++) {
+  /* 테스트를 위해 데이터를 1000개정도 만들었다. */
+  channels.push({
+    id: i,
+    title: `title: ${i}`,
+    description: `desc: ${i}`,
+    createdAt: i,
+  });
+}
+
+const ItemContainer = styled.TouchableOpacity`
+  /* ItemComponent를 감싸기 위해 ItemContainer를 만들어주었다. 그리고 항목을 클릭해서 채널로 이동해야하니 TouchableOpacity를 사용하였다. */
+  flex-direction: row;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-color: ${({ theme }) => theme.itemBorder};
+  padding: 15px 20px;
+`;
+
+const ItemTextContainer = styled.TouchableOpacity`
+  /* 제목과 설명을 감싸줄 컨테이너를 만들었다. */
+  flex: 1;
+  flex-direction: column;
+`;
+
+const ItemTitle = styled.Text`
+  /* 제목을 렌더링할 ItemTitle components를 만들었다. */
+  font-size: 20px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.item};
+`;
+
+const ItemDesc = styled.Text`
+  font-size: 16px;
+  margin-top: 5px;
+  color: ${({ theme }) => theme.itemDesc};
+`;
+
+const ItemTime = styled.Text`
+  font-size: 12px;
+  color: ${({ theme }) => theme.itemTime};
+`;
+
+const ItemIcon = styled(MaterialIcons).attrs(({ theme }) => ({
+  name: 'keyboard-arrow-right',
+  size: 24,
+  color: theme.itemIcon,
+}))``;
+
+const Item = React.memo(
+  /* React.memo는 컨포넌트에 변화가없을때 relandering을 방지해주는 역할을한다. */
+  ({ item: { id, title, description, createdAt }, onPress }) => {
+    console.log(id);
+
+    return (
+      <ItemContainer>
+        <ItemTextContainer>
+          <ItemTitle>{title}</ItemTitle>
+          <ItemDesc>{description}</ItemDesc>
+        </ItemTextContainer>
+        <ItemTime>{createdAt}</ItemTime>
+        <ItemIcon />
+      </ItemContainer>
+    );
+  }
+);
 
 const Container = styled.View`
-    flex: 1;
-    background-color: white;
+  flex: 1;
+  background-color: white;
 `;
 
 const StyledText = styled.Text`
-    font-size: 30px;
+  font-size: 30px;
 `;
 
-const ChannelList = ({navigation}) => {
-    return(
-        <Container>
-            <StyledText>Channel List</StyledText>
-            <Button title="Channel" onPress={() => navigation.navigate('Channel')} />
-        </Container>
-    );
+const ChannelList = ({ navigation }) => {
+  return (
+    <Container>
+      <FlatList /* FlatList에는 렌더링할 데이터를 가지고있는 배열을 data에 전달하고 renderItem에는 항목을 렌더링하기위해 컨포넌트를 반환하는 함수를 전달해야한다. */
+        data={channels}
+        renderItem={({ item }) => <Item item={item} />}
+        /* renderItem에 설정된 함수의 파라미터로 전달되는 내용에서 item이라는 프로퍼티에 데이터에 설정한 목록의 항목이 전달된다. 이 item을 item props로 전달했다. */
+        keyExtractor={(item) => item['id'].toString()}
+        /* flatlist에 keyExtractor를 이용하면 쉽게 key를 지정할 수 있다. 
+        keyExtractor에는 함수를 전달해야하는데 함수의 파라미터로 항목이 전달된다. 
+        함수를 통해 어떠한 값을 키로 사용할지 알려주면 flatlist가 자동으로 key를 설정한다. */
+        windowSize={5}
+        /* windowSize를 줄이면 렌더링되는 숫자도 줄어든다. 
+        이렇게 windowSize를 조절하면 미리불러와서 렌더링하는 컨포넌트의 숫자를 줄일 수 있다. 
+        그리고 이를통해 메모리의 소비를 줄이고 성능을 향상시킬 수 있다. 
+        하지만 빠르게 스크롤을 하다보면 데이터를 불러오기전에 스크롤이 해당위치에 도달해서 순간적으로 데이터가 보이지 않을 수 있다는 단점이있다. 
+        windowSize를 무조건 줄이기보다는 화면에 따라 적절한값을 설정하는것이 중요하다. */
+      />
+    </Container>
+  );
 };
 
 export default ChannelList;
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,25 +1,26 @@
 const Colors = {
-    white: '#ffffff',
-    black: '#111111',
-    main: '#F6EBFC',
-    grey_0: '#d5d5d5', /* Image의 배경색으로 사용할 색이다.*/
-    grey_1: '#a6a6a6',
-    grey_2: '#f4f4f4',
-    red: '#e84118', /* 에러메시지에 사용할 색을 정의해주었다. */
-    blue: '#0080ff'
-  };
+  white: '#ffffff',
+  black: '#111111',
+  main: '#F6EBFC',
+  grey_0: '#d5d5d5' /* Image의 배경색으로 사용할 색이다.*/,
+  grey_1: '#a6a6a6',
+  grey_2: '#f4f4f4',
+  red: '#e84118' /* 에러메시지에 사용할 색을 정의해주었다. */,
+  blue: '#0080ff',
+};
 
-  export const theme = {
-    background: Colors.main,
-    bar: Colors.white,
-    text: Colors.black,
-    errorText: Colors.red,
+export const theme = {
+  background: Colors.main,
+  bar: Colors.white,
+  text: Colors.black,
+  errorText: Colors.red,
 
-    // Button
+  // Button
   btnBackground: Colors.white,
   btnTitle: Colors.black,
-  btnTextLink: Colors.black /* 배경색이 없는 버튼을 이용할때 사용할 색을 정의한 코드 */,
-//   btnSignout: Colors.red,
+  btnTextLink:
+    Colors.black /* 배경색이 없는 버튼을 이용할때 사용할 색을 정의한 코드 */,
+  //   btnSignout: Colors.red,
 
   // Image
   imgBackground: Colors.grey_0,
@@ -41,4 +42,10 @@ const Colors = {
   //Tab
   tabBtnActive: Colors.blue,
   tabBtnInactive: Colors.grey_1,
-  };
+
+  // List - Item
+  itemBorder: Colors.grey_0,
+  itemTime: Colors.grey_1,
+  itemDesc: Colors.grey_1,
+  itemIcon: Colors.text,
+};
